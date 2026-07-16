a128fc3e012619068f192f41c0bfd9ad28462cb6
Fedor Antonenko <cupofmilk80@gmail.com>
2026-07-16 12:21:58 +0300

    1) обновлен RequestMapper под новую модель с использованием айди

--- event-service/src/main/java/ru/cupofmilk/mapper/RequestMapper.java
+++ event-service/src/main/java/ru/cupofmilk/mapper/RequestMapper.java
@@ -6,17 +6,21 @@ import ru.cupofmilk.model.event.Request;
 public class RequestMapper {
     public static RequestDto toDto(Request request) {
         RequestDto requestDto = new RequestDto();
-        requestDto.setEvent(EventMapper.toDto(request.getEvent()));
+        if (request.getEvent() != null) {
+            requestDto.setEvent(EventMapper.toDto(request.getEvent()));
+        }
+        requestDto.setEventId(request.getEventId());
         requestDto.setNow(request.isNow());
-
         return requestDto;
     }
 
     public static Request toModel(RequestDto requestDto) {
         Request request = new Request();
-        request.setEvent(EventMapper.toModel(requestDto.getEvent()));
+        if (requestDto.getEvent() != null) {
+            request.setEvent(EventMapper.toModel(requestDto.getEvent()));
+        }
+        request.setEventId(requestDto.getEventId());
         request.setNow(requestDto.isNow());
-
         return request;
     }
 }
